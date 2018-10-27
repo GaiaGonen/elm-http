1ee69ecd0a3eb6109e54e7f840122d9d51ece357
GG <gaiagonen@gmail.com>
2018-10-27 13:00:42 +0300

    basic main structure from elm official guide

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,10 @@
 import Browser
 import Html exposing (..)
-
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+import Url.Builder as Url
 
 
 --MAIN
@@ -16,29 +20,73 @@ main =
 -- MODEL
 
 type alias Model =
-  {
+  { topic : String
+  , url : String
   }
 
-init :
+init : () -> (Model, Cmd Msg)
 init _ =
+  ( Model "cat" "waiting.gif"
+  , getRandomGif "cat"
+  )
 
 
 --UPDATE
 
 type Msg
-  =
+  = MorePlease
+  | NewGif (Result Http.Error String)
 
 update :
-update msg model =
+update msg model = Msg -> Model -> (Model, Cmd Msg)
   case msg of
+    MorePlease ->
+      ( model
+      , getRandomGif model.topic
+      )
+
+      NewGif result ->
+        case result of
+          Ok newUrl ->
+            ( { model | url = newUrl }
+            , Cmd.none
+            )
+
+          Err ->
+            ( model
+            , Cmd.none
+            )
 
 --SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-
+  Sub.none
 
 --VIEW
 
 view : Model -> Html Msg
 view model =
+  div []
+    [ h2 [] [ text model.topic ]
+    , button [ onClick MorePlease ] [ text "More Please!" ]
+    , br [] []
+    , img [ src model.url ] []
+    ]
+
+-- HTTP
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  Http.send NewGif (Http.get (toGiphyUrl topic) gifDecoder)
+
+toGiphyUrl : String -> String
+toGiphyUrl topic =
+  Url.crossOrigin "https://api.giphy.com" ["v1","gifs","random"]
+    [ Url.string "api_key" "dc6zaTOxFJmzC"
+    , Url.string "tag" topic
+    ]
+
+gifDecoder : Decode.Decoder String
+gifDecoder =
+  Decode.field "data" (Decode.field "image_url" Decode.string)
